41641ac6916f5652875442d5409290af5cb41349
Khelmaron <jenskoller@gmx.net>
2018-04-08 18:12:01 +0200

    sss

--- Src/Spielerverwaltung/forms.py
+++ Src/Spielerverwaltung/forms.py
@@ -1,8 +1,18 @@
 from django import forms
+from django.forms import ModelForm
+from Spielerverwaltung.models import Raid
 
 class CreatePlayerForm(forms.Form):
     name = forms.CharField(max_length = 50, label = 'Neuer Spieler')
 
+class CreateRaidForm(ModelForm):
+    class Meta:
+        model = Raid
+        fields = ['Raidstart','Raidende','Teilnehmer']
+        widgets = {
+            'Raidstart' : forms.DateTimeInput(attrs= {'type' : 'datetime-local'}),
+            'Raidende'  : forms.DateTimeInput(attrs= {'type' : 'datetime-local'})
+        }
 ##class linkPlayer(forms.Form):
 
 ##    Spieler = forms.
